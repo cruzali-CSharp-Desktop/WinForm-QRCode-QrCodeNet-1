30507d4dead68398c721dbe005ee5358ac2c5163
Ali <ali.cruz@me.com>
2023-05-05 10:59:47 -0600

    Carga inicial

---
+++ GeneraQRForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace GeneraQR_01
+{
+    partial class GeneraQRForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.BtnGenerar = new System.Windows.Forms.Button();
+            this.TxbTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Enabled = false;
+            this.BtnGuardar.Location = new System.Drawing.Point(165, 277);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(81, 28);
+            this.BtnGuardar.TabIndex = 9;
+            this.BtnGuardar.Text = "Guardar QR";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // BtnGenerar
+            // 
+            this.BtnGenerar.Location = new System.Drawing.Point(42, 278);
+            this.BtnGenerar.Name = "BtnGenerar";
+            this.BtnGenerar.Size = new System.Drawing.Size(81, 28);
+            this.BtnGenerar.TabIndex = 8;
+            this.BtnGenerar.Text = "Generar QR";
+            this.BtnGenerar.UseVisualStyleBackColor = true;
+            this.BtnGenerar.Click += new System.EventHandler(this.BtnGenerar_Click);
+            // 
+            // TxbTexto
+            // 
+            this.TxbTexto.Location = new System.Drawing.Point(42, 248);
+            this.TxbTexto.Name = "TxbTexto";
+            this.TxbTexto.Size = new System.Drawing.Size(201, 20);
+            this.TxbTexto.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "URL / Texto";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GeneraQRForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 328);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.BtnGenerar);
+            this.Controls.Add(this.TxbTexto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GeneraQRForm";
+            this.Text = "GeneraQRForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.Button BtnGenerar;
+        private System.Windows.Forms.TextBox TxbTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ GeneraQRForm.cs
@@ -0,0 +1,59 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneraQR_01
+{
+    public partial class GeneraQRForm : Form
+    {
+        public GeneraQRForm()
+        {
+            InitializeComponent();
+            BtnGuardar.Enabled = false;
+        }
+
+        private void BtnGenerar_Click(object sender, EventArgs e)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(TxbTexto.Text.Trim(), out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400,QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+
+            MemoryStream memoryStream = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, memoryStream);
+            //var imageTMP = new Bitmap(imageTMP, new Size(new Point(200, 200)));
+
+            var imageTMP = new Bitmap(memoryStream);
+            var imagen = new Bitmap(imageTMP, new Size(new Point(200, 200)));
+            pictureBox1.BackgroundImage = imagen;
+
+            BtnGuardar.Enabled = true;
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            using (Image image = (Image)pictureBox1.BackgroundImage.Clone())
+            {
+                SaveFileDialog resDialog = new SaveFileDialog();
+                resDialog.AddExtension = true;
+                resDialog.Filter = "Image PNG (*.png)|*.png";
+
+                if (resDialog.ShowDialog() == DialogResult.OK)
+                {
+                    image.Save(resDialog.FileName, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
